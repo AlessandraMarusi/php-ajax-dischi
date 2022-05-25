70872c2463f5b75be3bcf01c04eecf1f038f2143
AlessandraMarusi <alessandra.marusi@gmail.com>
2022-05-25 17:35:03 +0200

    Scaffolding

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+include __DIR__. '/database.php'
+
+?>
+
+
+
+<?php include __DIR__. '/partials/header.php' ?>
+    <main>
+        <div class="row">
+            <div class="col">
+                <span>Prova Prova Prova Prova Prova Prova Prova Prova Prova Prova Prova Prova Prova Prova Prova Prova </span>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
