e7d5e3376ee6079b91b86564235cb40e742f7ef4
AlessandraMarusi <alessandra.marusi@gmail.com>
2022-05-25 23:50:29 +0200

    Aggiunto bonus

--- script/script.js
+++ script/script.js
@@ -2,14 +2,32 @@ const app = new Vue({
     el: '#app',
     data:{
         dischi:[],
+        genres:[],
+        checkedGenre: '',
+        apiPath: './server.php'
     },
     methods:{
-
+        filter(){
+            this.getData(this.checkedGenre);
+        },
+        getData(genre=null){
+            let path = this.apiPath;
+            if(genre){
+              path = `${this.apiPath}?genre=${genre}`;
+            }
+            axios.get(path).then((res)=>{
+                this.dischi = res.data;
+                if(this.genres.length < 1){
+                    this.dischi.forEach((disco)=>{
+                        if(!this.genres.includes(disco.genre)){
+                            this.genres.push(disco.genre);
+                        }
+                    })
+                }
+            })
+        }
     },
     mounted(){
-        axios.get('server.php').then((res)=>{
-            this.dischi = res.data;
-            console.log(this.dischi);
-        })
+        this.getData();
     }
 });
--- server.php
+++ server.php
@@ -1,5 +1,16 @@
 <?php 
 include __DIR__.'/database.php';
+$result = [];
+if(isset($_GET['genre'])){
+    foreach($dischi as $val){
+        if($val['genre'] == $_GET['genre']){
+            $result[] = $val;
+        }
+    }
+} else {
+    $result = $dischi;
+}
+
 header('Content-Type: application/json');
-echo json_encode($dischi);
+echo json_encode($result);
 ?>
